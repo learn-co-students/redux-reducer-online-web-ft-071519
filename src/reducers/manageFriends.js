c90c416e5fe5f68c9200a86d9ac96fdbfd18b0ba
Nicole Dow <nikki.dow71@gmail.com>
2019-11-26 10:42:36 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+  switch(action.type) {
+      case 'ADD_FRIEND':
+          let newFriend = {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id};
+          state.friends.push(newFriend);
+          return state;
+      case 'REMOVE_FRIEND':
+          //let olderDog = Object.assign({}, dog, {age: dog.age + 1})
+          let withRemovedFriend = [];
+          state.friends.forEach(friend => {
+              if (friend.id != action.id) {
+                  withRemovedFriend.push(friend);
+              }
+          })
+          return Object.assign({}, {friends: withRemovedFriend});
+      default:
+          return state;
+  }
 }
