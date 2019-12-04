554be5a9a40c8b5f7ef378733b3c534a61c18679
Audrea Cook <audreacwork@gmail.com>
2019-12-04 15:54:30 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend] };
+    case 'REMOVE_FRIEND':
+      let newFriends = [...state.friends];
+      let friendIndex = newFriends.findIndex(f => f.id === action.id);
+      newFriends.splice(friendIndex, 1);
+      return {friends: newFriends};
+    default:
+      return state
+  }
 }
